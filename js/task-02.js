2fc637696b816d69ed874ae3a835f0015dd92d33
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-05-08 12:21:49 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ul = document.querySelector(`#ingredients`);
+const liElements = [];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const li = document.createElement(`li`);
+  li.textContent = ingredients[i];
+  li.classList.add(`item`);
+  liElements.push(li);
+}
+ul.append(...liElements);
