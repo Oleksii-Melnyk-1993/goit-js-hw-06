23bd3272c6849b78e61ea0fd5442231db13fa467
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-05-11 20:43:03 +0300

    finish?

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(`.login-form`);
+form.addEventListener(`submit`, onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
 
+  const emailForm = form.elements.email;
+  const passForm = form.elements.password;
+  if (emailForm.value === "" || passForm.value === "") {
+    alert(`Fill the form`);
+  } else {
+    const formData = {
+      email: emailForm.value,
+      password: passForm.value,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const bodyEl = document.querySelector(`body`);
+const colorShows = document.querySelector(`.color`);
+const changeColor = document.querySelector(`.change-color`);
+changeColor.addEventListener("click", () => {
+  colorShows.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
